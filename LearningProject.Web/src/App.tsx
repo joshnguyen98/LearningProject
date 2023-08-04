c5267919bca6c3900535a82bf45ab0aae062d914
joshnguyen98 <josh.nguyen@kpmg.co.uk>
2023-08-04 11:04:50 +0100

    sorting out nonce

--- LearningProject.Web/src/App.tsx
+++ LearningProject.Web/src/App.tsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "./api.ts";
 
-// type User = {
-//   id: number;
-//   username: string;
-//   email: string;
-// };
-
 function App() {
   const [users, setUsers] = useState([]);
 
@@ -20,11 +14,7 @@ function App() {
     <div>
       <ul>
         {users.map((user) => {
-          return (
-            <li key={user["id"]}>
-              {user["username"]}, {user["email"]}
-            </li>
-          );
+          return <li key={user["id"]}>{user["username"]}</li>;
         })}
       </ul>
     </div>
